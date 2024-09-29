7e9c96eaff82045f159709c9d5c55c739b5877cb
Andrés Balladares Flores <168479277+BalladaresF@users.noreply.github.com>
2024-09-29 07:53:46 -0600

    Add files via upload

---
+++ AlgoritmosOrdenacion/src/Clases/Main.java
@@ -0,0 +1,25 @@
+/*
+    Se ha creado una pantalla donde se generan 2000 números aleatorios de tipo double, los cuales se
+    almacenan en un arreglo primitivo de tipo double[ ] valores = new double[2000].
+    Se utilizan los siguientes algoritmos:
+        ● Burbuja (Bubble Sort).
+        ● Inserción (Insertion Sort).
+        ● Mezclas (Merge Sort).
+        ● Rápido (Quicksort).
+ */
+package Clases;
+
+/**
+ *
+ * @author Andres B
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Pantalla Pantalla=new Pantalla();
+        Pantalla.setVisible(true); 
+    } 
+}
