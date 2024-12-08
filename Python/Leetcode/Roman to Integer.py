7c8033cd7a3b2f47b0751e18b3bbec5aa970fc29
Andrés Balladares Flores <168479277+BalladaresF@users.noreply.github.com>
2024-12-07 21:31:25 -0600

    Update Roman to Integer.py

--- Python/Leetcode/Roman to Integer.py
+++ Python/Leetcode/Roman to Integer.py
@@ -1,5 +1,7 @@
+# Se busca convertir números romanos a cifras arábigas.
+
 # I = 1, V = 5, X = 10, L = 50
-#C = 100, D = 500, M = 1000
+# C = 100, D = 500, M = 1000
 
 A = "IV"        # 4
 B = "MCMXCIV"   # 1994
